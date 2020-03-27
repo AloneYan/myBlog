377a4dd1c7a38abd15b1ce83f66f518f2fae63df
丁少华 <dingshao@aegis-data.cn>
2020-03-27 22:54:50 +0800

    fix:修复不能默认选中nav的bug

--- src/pages/header/nav/index.tsx
+++ src/pages/header/nav/index.tsx
@@ -1,76 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./style.less";
 import { Menu, Row, Col } from "antd";
 import { Link } from "react-router-dom";
-import csdnImg from "@assets/images/csdn.jpg";
-import githubImg from "@assets/images/github.jpg";
-import sinaImg from "@assets/images/sina.jpg";
-class Nav extends React.Component {
-  state = {
-    current: "mark"
-  };
+import { navRight, menus } from './model';
+import { withRouter } from 'react-router-dom';
 
-  handleClick = (e: any) => {
-    console.log("click ", e);
-    this.setState({
-      current: e.key
-    });
-  };
+const Nav = (props: any) => {
+  const [current, setCurrent] = useState<string>('mark');
 
-  render() {
-    return (
-      <div className={style.nav}>
-        <Row className={style.navCont}>
-          <Col span={16} className={style.navLeft}>
-            <Menu
-              onClick={this.handleClick}
-              selectedKeys={[this.state.current]}
-              mode="horizontal"
-            >
-              <Menu.Item key="mark">
-                <Link to="/">兔子の文档</Link>
+  useEffect(() => {
+    const currNavIdx = menus.findIndex(item => item.path === props.location.pathname);
+    setCurrent(menus[currNavIdx].id)
+  }, [])
+
+  return (
+    <div className={style.nav}>
+      <Row className={style.navCont}>
+        <Col span={16} className={style.navLeft}>
+          <Menu onClick={(e) => { setCurrent(e.key) }}
+            selectedKeys={[current]}
+            mode="horizontal">
+            {menus.map(item => (
+              <Menu.Item key={item.id}>
+                <Link to={item.path}>{item.title}</Link>
               </Menu.Item>
-              <Menu.Item key="book">
-                <Link to="/book"> 兔子の书单</Link>
-              </Menu.Item>
-              <Menu.Item key="every">
-                <Link to="/every"> 兔子の日常</Link>
-              </Menu.Item>
-              <Menu.Item key="write">
-                <Link to="/write"> 兔子の留言板</Link>
-              </Menu.Item>
-            </Menu>
-          </Col>
-          <Col span={8} className={style.navRight}>
-            <a
-              className={style.navRightImg}
-              href="https://github.com/AloneYan"
-              title="去github看看"
+            ))}
+          </Menu>
+        </Col>
+        <Col span={8} className={style.navRight}>
+          {navRight.map(item => (
+            <a className={style.navRightImg}
+              href={item.href}
               target="blank"
-            >
-              <img src={githubImg} alt="github"></img>
+              title={item.title}
+              key={item.href}>
+              <img src={item.img} alt={item.title} />
             </a>
-            <a
-              className={style.navRightImg}
-              href="https://blog.csdn.net/YanH_an"
-              title="去csdn看看"
-              target="blank"
-            >
-              <img src={csdnImg} alt="csdn"></img>
-            </a>
-            <a
-              className={style.navRightImg}
-              href="https://www.weibo.com/5194163137/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1"
-              title="去微博看看"
-              target="blank"
-            >
-              <img src={sinaImg} alt="新浪微博"></img>
-            </a>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+          ))}
+        </Col>
+      </Row>
+    </div>
+  );
 }
+const cmp = withRouter(Nav);
+export default cmp;
 
-export default Nav;
---
+++ src/pages/header/nav/model.ts
@@ -0,0 +1,44 @@
+import csdnImg from "@assets/images/csdn.jpg";
+import githubImg from "@assets/images/github.jpg";
+import sinaImg from "@assets/images/sina.jpg";
+
+export const navRight = [
+  {
+    href:"https://github.com/AloneYan",
+    title:"去github看看",
+    img:githubImg
+  },
+  {
+    href:"https://blog.csdn.net/YanH_an",
+    title:"去csdn看看",
+    img:csdnImg
+  },
+  {
+    href:"https://www.weibo.com/5194163137/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1",
+    title:"去微博看看",
+    img:sinaImg
+  }
+]
+
+export const menus = [
+  {
+    id:'mark',
+    path:'/',
+    title:'兔子の文档'
+  },
+  {
+    id:'book',
+    path:'/book',
+    title:'兔子の书单'
+  },
+  {
+    id:'every',
+    path:'/every',
+    title:'兔子の日常'
+  },
+  {
+    id:'write',
+    path:'/write',
+    title:'兔子の留言板'
+  }
+]
