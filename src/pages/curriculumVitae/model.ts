10c9df83a1194c4b69f5ce311cde4d4b8174d399
AloneYan <869043928@qq.com>
2020-07-22 17:44:51 +0800

    ✨简历

---
+++ src/pages/curriculumVitae/model.ts
@@ -0,0 +1,41 @@
+export const progressColor = [
+  {
+    type: "Html/Html5",
+    colorStar: "#fbc2eb",
+    colorEnd: "#a18cd1",
+    percent: 88,
+    des: "大家都会。",
+  },
+  {
+    type: "Css/Less",
+    colorStar: "#F9FEA5",
+    colorEnd: "#20E2D7",
+    percent: 86,
+    des:
+      "这两个建立在设计大佬的基础上，我对美感只有一点点，but只要别人做的出我就可以，好不好看只是时间问题。",
+  },
+  {
+    type: "JavaScript/TyptScript",
+    colorStar: "#fff1eb",
+    colorEnd: "#ace0f9",
+    percent: 80,
+    des:
+      "本兔的博客就是使用ts写，不能说对js/ts原理精通，只能说是常常使用乃吃饭之根本。",
+  },
+  {
+    type: "Vue/React/Angular",
+    colorStar: "#fbc2eb",
+    colorEnd: "#a18cd1",
+    percent: 70,
+    des:
+      "本兔的博客就是由react搭建，所以最熟悉react。vue工作中用过，写过两个项目。angular只会一点ng1，单独搭建完全不行。",
+  },
+  {
+    type: "小程序/H5",
+    colorStar: "#F9FEA5",
+    colorEnd: "#20E2D7",
+    percent: 90,
+    des:
+      "这个太太太太常用了，独立完成5个以上数量的小程序，写起来速度飞快，可以接入微信支付定位即时通讯直播等等等。h5也写了好多。",
+  },
+];
